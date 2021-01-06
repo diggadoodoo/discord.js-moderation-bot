b5580c7027ee9b91c58facb035b6f74e9772be1c
Olsen <44035165+fifthsonofole@users.noreply.github.com>
2021-01-06 14:53:23 +0000

    Update bot.js
    Updated to v12, 
    
    Uptime and mute command gone temporarily

--- bot.js
+++ bot.js
@@ -4,206 +4,113 @@ const config = require("./config.json")
 
 bot.login(config.token)
 
-
 bot.on("ready", () => {
-    console.log("Ready")
-    bot.user.setStatus("online")
-    bot.user.setActivity(":smiley:")
-});
-
-bot.on("reconnecting", () => {
-    console.log("Reconnecting")
-});
-
-bot.on("guildCreate", guild => {
-    guild.createRole({ name: "Muted", color: "#313131" })
-    console.log("Joined a new server:" + guild.name)
-    console.log("It has " + guild.memberCount + " members ;)")
-});
-
-bot.on("guildDelete", guild => {
-    console.log("Left the server:" + guild.name)
+    console.log("Loaded up!")
 });
 
 bot.on("message", message => {
-
     if (message.author.bot) return;
     if (message.content.indexOf(config.prefix) !== 0) return;
+
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase()
 
     if (command === "help") {
-        const fayer = bot.users.get("304307395289415680")
-        const helpEmbed = new Discord.RichEmbed()
-            .setTitle(`My commands list | prefix \`${config.prefix}\``)
-            .addField("``ping``", "Your basic ping pong command")
-            .addField("``uptime``", "Seeing the bot's uptime")
-            .addField("``kick``", "Kicks the user mentioned, you know, use it whenever needed")
-            .addField("``ban``", "Bans the user mentioned, be careful with the ban hammer, it is heavy")
-            .addField("``mute``", "Finally shuts up that annoying user.")
-            .addField("``unmute``", "Unsilence the annoying user")
-            .addField("``add``", "Adds role to a user, hooray :tada:")
-            .addField("``remove``", "Removes role from user, sadface")
-            .addField("``rps``", "a basic rock paper scissors commands, no cheating!")
-            .setThumbnail(bot.avatarURL)
-            .setColor("#020101")
+        const helpEmbed = new Discord.MessageEmbed()
+            .setTitle(`${bot.user.username}'s commands`)
+            .setDescription(`**Prefix:** ${config.prefix}`)
+            .addField(`\`ping\``, `Check your bot's ping`)
+            .addField(`\`kick\``, `Usage: **${config.prefix}kick [@User]**\n**${config.prefix}kick [@User][Reason]**`)
+            .addField(`\`ban\``, `Usage: **${config.prefix}ban [@User]**\n**${config.prefix}ban [@User][Reason]**`)
+            .addField(`\`add\``, `Adds a role to a user \nUsage: **${config.prefix}add [@User] [Role]**`)
+            .addField(`\`remove\``, `Removes a role from a user \nUsage: **${config.prefix}remove [@User] [Role]**`)
+            .addField(`\`rps\``, `Play rock paper scissors`)
         message.channel.send(helpEmbed)
+    }
 
+    if (command === "ping") {
+        message.channel.send(`Pong **(${Date.now() - message.createdTimestamp}ms)**`)
     }
 
     if (command === "kick") {
         if (!message.member.hasPermission('KICK_MEMBERS'))
-            return message.channel.send(":no_entry: Insufficient permissions")
+            return message.channel.send("Insufficient permissionss (Requires `KICK_MEMBERS`)")
         const member = message.mentions.members.first();
         if (!member)
-            return message.channel.send(":no_entry: No user mentioned.")
-        const reason = args.slice(1).join(" ") 
+            return message.channel.send("You have not mentioned a user")
         if (!member.kickable)
-            return message.channel.send(":no_entry: I cannot kick this user.")
+            return message.channel.send("This user is unkickable")
+        const reason = args.slice(1).join(" ")
         if (member) {
-            if (!reason) {
-                return member.kick().then(member => {
-                    message.channel.send(`${member.user.tag} was kicked by ${message.author}, no reason was provided.`);
-                })
-            }
-            if (reason) {
-                member.kick().then(member => {
-                    message.channel.send(`${member.user.tag} was kicked by ${message.author}, no reason was provided.`);
-                })
-            }
+            if (!reason) return member.kick().then(member => {
+                message.channel.send(`${member.user.tag} was kicked, no reason was provided`);
+            })
+
+            if (reason) return member.kick().then(member => {
+                message.channel.send(`${member.user.tag} was kicked for ${reason}`);
+            })
         }
     }
+
     if (command === "ban") {
         if (!message.member.hasPermission('BAN_MEMBERS'))
-            return message.channel.send(":no_entry: Insufficient permissions")
+            return message.channel.send("Insufficient permissionss (Requires `BAN_MEMBERS`)")
         const member = message.mentions.members.first();
         if (!member)
-            return message.channel.send(":no_entry: No user mentioned.")
-        const reason = args.slice(1).join(" ")
-        if (!member.kickable)
-            return message.channel.send(":no_entry: I cannot ban this user.")
-        if (member) {
-            if (!reason) {
-                return member.ban().then(member => {
-                    message.channel.send(`${member.user.tag} was banned by ${message.author}, no reason was provided.`)
-                })
-            }
-            if (reason) {
-                member.ban().then(member => {
-                    message.channel.send(`${member.user.tag} was banned by ${message.author} for ${reason}.`)
-                })
-            }
-        }
-    }
-
-    if (command === "mute") {
-        if (!message.member.hasPermission('MANAGE_MESSAGES'))
-            return message.channel.send(":no_entry: Insufficient permissions")
-        const mute = message.guild.roles.find(r => r.name === "Muted");
-        message.guild.channels.forEach(channel => {
-            channel.overwritePermissions(mute, { SEND_MESSAGES: false })
-        })
-        if (!mute)
-            return message.channel.send(":no_entry: No mute role")
-        const member = message.mentions.members.first();
-        if (!member)
-            return message.channel.send(":no_entry: No user mentioned")
-        if (member.roles.some(r => ["Muted"].includes(r.name)))
-            return message.channel.send(":no_entry: User is already muted")
-        if (member.id === message.author.id) {
-            return message.channel.send("I know you'd find it funny to mute yourself but sorry, no can do")
-        }
-        if (member.id === bot.user.id) {
-            return message.channel.send("Very funny")
-        }
+            return message.channel.send("You have not mentioned a user")
+        if (!member.bannable)
+            return message.channel.send("This user is unbannable")
         const reason = args.slice(1).join(" ")
         if (member) {
-            if (!reason) {
-                return member.addRole(mute).then(member => {
-                    message.channel.send(`${member.user.tag} was muted by ${message.author}, no reason was provided`)
+            if (!reason) return member.ban().then(member => {
+                message.channel.send(`${member.user.tag} was banned, no reason was provided`);
+            })
 
-                })
-
-            }
-            if (reason) {
-                member.addRole(mute).then(member => {
-                    message.channel.send(`${member.user.tag} was muted by ${message.author} for ${reason}`).catch(error => {
-                        message.channel.send(":no_entry: Cannot mute this user")
-                    })
-                })
-            }
-        }
-
-    }
-
-    if (command === "unmute") {
-        if (!message.member.hasPermission('MANAGE_MESSAGES'))
-            return message.channel.send(":no_entry: Insufficient permissions")
-        const mute = message.guild.roles.find(r => r.name === "Muted");
-        if (!mute)
-            return message.channel.send(":no_entry: No mute role")
-        const member = message.mentions.members.first();
-        if (!member)
-            return message.channel.send(":no_entry: No user mentioned")
-        if (!member.roles.some(r => ["Muted"].includes(r.name)))
-            return message.channel.send(":no_entry: User is not muted")
-        if (member) {
-            member.removeRole(mute).then(member => {
-                message.channel.send(`${member.user.tag} was unmuted by ${message.author}`)
-
+            if (reason) return member.ban(reason).then(member => {
+                message.channel.send(`${member.user.tag} was banned for ${reason}`);
             })
         }
     }
+
     if (command === "add") {
-        if (!message.member.hasPermission("MANAGE_ROLES"))
-            return message.channel.send(":no_entry: Insufficient permissions")
+        if (!message.member.hasPermission('MANAGE_ROLES'))
+            return message.channel.send("Insufficient permissionss (Requires `MANAGE_ROLES`)")
         const member = message.mentions.members.first()
         if (!member)
-            return message.channel.send(":no_entry: No user mentioned")
+            return message.channel.send("You have not mentioned a user")
         const add = args.slice(1).join(" ")
         if (!add)
-            return message.channel.send(":no_entry: No role said")
-        const roleAdd = message.guild.roles.find(role => role.name === add);
+            return message.channel.send("You have not specified a role")
+        const roleAdd = message.guild.roles.cache.find(role => role.name === add)
         if (!roleAdd)
-            return message.channel.send(":no_entry: Role does not exist")
-            if (member.roles.has(roleAdd.id)){
-            return message.channel.send(":no_entry: User already has role")
-            }
-        if (member) {
-            member.addRole(roleAdd).catch((error) =>{
-                message.channel.send(":cry: Error")
-            }).then((member) => {
-                message.channel.send(`:thumbsup: ${roleAdd} added to ${member.displayName}`)
-            })
-
-        }
+            return message.channel.send("This role does not exist")
+        if (member.roles.cache.get(roleAdd.id))
+            return message.channel.send(`This user already has the ${add} role`)
+        member.roles.add(roleAdd.id).then((member) => {
+            message.channel.send(`${add} added to ${member.displayName}`)
+        })
     }
+
     if (command === "remove") {
-        if (!message.member.hasPermission("MANAGE_ROLES"))
-            return message.channel.send(":no_entry: Insufficient permissions")
+        if (!message.member.hasPermission('MANAGE_ROLES'))
+            return message.channel.send("Insufficient permissionss (Requires `MANAGE_ROLES`)")
         const member = message.mentions.members.first()
         if (!member)
-            return message.channel.send(":no_entry: No user mentioned")
+            return message.channel.send("You have not mentioned a user")
         const remove = args.slice(1).join(" ")
         if (!remove)
-            return message.channel.send(":no_entry: No role said")
-        const roleRemove = message.guild.roles.find(role => role.name === remove);
+            return message.channel.send("You have not specified a role")
+        const roleRemove = message.guild.roles.cache.find(role => role.name === remove)
         if (!roleRemove)
-            return message.channel.send(":no_entry: Role does not exist")
-            if (!member.roles.has(roleRemove.id)){
-            return message.channel.send(":no_entry: User already does not have role")
-            }
-        if (member) {
-            member.removeRole(roleRemove).catch((err) =>{
-                message.channel.send(":cry: Error")
-            }).then((member) => {
-                message.channel.send(`:thumbsup: ${roleRemove} removed from ${member.displayName}`)
-            })
-
-        }
+            return message.channel.send("This role does not exist")
+        if (!member.roles.cache.get(roleRemove.id))
+            return message.channel.send(`This user does not have the ${remove} role`)
+        member.roles.remove(roleRemove.id).then((member) => {
+            message.channel.send(`${remove} removed from ${member.displayName}`)
+        })
     }
 
-    if(command === "rps"){
+    if (command === "rps") {
         const options = [
             "rock :shell: ",
             "paper :newspaper2:",
@@ -213,15 +120,4 @@ bot.on("message", message => {
         message.channel.send(`You got ${option}`)
     }
 
-    if (command === "uptime") {
-        let totalSeconds = (bot.uptime / 1000);
-        let days = Math.floor(totalSeconds / 86400);
-        let hours = Math.floor(totalSeconds / 3600);
-        totalSeconds %= 3600;
-        let minutes = Math.floor(totalSeconds / 60);
-        let seconds = totalSeconds / 60;
-        message.channel.send(`:low_brightness: **Uptime:** ${days} days, ${hours} hours and ${minutes} mins`)
-
-    }
 });
-
