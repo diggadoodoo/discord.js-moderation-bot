c6d9e2221ae1e9196cb987230491bedefb11d469
SS <74042715+choppymaster@users.noreply.github.com>
2021-01-09 13:37:18 +0530

    Add say command and add auto delete
    This PR adds the ability to auto delete error messages as mentioned in #7 . The time to auto delete is set to 30000 ms(30 seconds), You can change it you want. This PR also adds ability to repeat the saying with a command. For example,
    
    I say  `!say hello`
    And the bot will say `hello`.
    
    ^^^^^^^

--- bot.js
+++ bot.js
@@ -34,12 +34,18 @@ bot.on("message", message => {
 
     if (command === "kick") {
         if (!message.member.hasPermission('KICK_MEMBERS'))
-            return message.channel.send("Insufficient permissions (Requires permission `Kick members`)")
+            return message.channel.send("Insufficient permissions (Requires permission `Kick members`)").then(msg => {
+        msg.delete({ timeout: 30000 })
+    })
         const member = message.mentions.members.first();
         if (!member)
-            return message.channel.send("You have not mentioned a user")
+            return message.channel.send("You have not mentioned a user").then(msg => {
+        msg.delete({ timeout: 30000 })
+    })
         if (!member.kickable)
-            return message.channel.send("This user is unkickable")
+            return message.channel.send("This user is unkickable").then(msg => {
+        msg.delete({ timeout: 30000 })
+    })
         const reason = args.slice(1).join(" ")
         if (member) {
             if (!reason) return member.kick().then(member => {
@@ -54,12 +60,18 @@ bot.on("message", message => {
 
     if (command === "ban") {
         if (!message.member.hasPermission('BAN_MEMBERS'))
-            return message.channel.send("Insufficient permissions (Requires permission `Ban members`)")
+            return message.channel.send("Insufficient permissions (Requires permission `Ban members`)").then(msg => {
+        msg.delete({ timeout: 30000 })
+    })
         const member = message.mentions.members.first();
         if (!member)
-            return message.channel.send("You have not mentioned a user")
+            return message.channel.send("You have not mentioned a user").then(msg => {
+        msg.delete({ timeout: 30000 })
+    })
         if (!member.bannable)
-            return message.channel.send("This user is unbannable")
+            return message.channel.send("This user is unbannable").then(msg => {
+        msg.delete({ timeout: 30000 })
+    })
         const reason = args.slice(1).join(" ")
         if (member) {
             if (!reason) return member.ban().then(member => {
@@ -74,18 +86,28 @@ bot.on("message", message => {
 
     if (command === "add") {
         if (!message.member.hasPermission('MANAGE_ROLES'))
-            return message.channel.send("Insufficient permissions (Requires permission `Manage roles`)")
+            return message.channel.send("Insufficient permissions (Requires permission `Manage roles`)").then(msg => {
+        msg.delete({ timeout: 30000 })
+    })
         const member = message.mentions.members.first()
         if (!member)
-            return message.channel.send("You have not mentioned a user")
+            return message.channel.send("You have not mentioned a user").then(msg => {
+        msg.delete({ timeout: 30000 })
+    })
         const add = args.slice(1).join(" ")
         if (!add)
-            return message.channel.send("You have not specified a role")
+            return message.channel.send("You have not specified a role").then(msg => {
+        msg.delete({ timeout: 30000 })
+    })
         const roleAdd = message.guild.roles.cache.find(role => role.name === add)
         if (!roleAdd)
-            return message.channel.send("This role does not exist")
+            return message.channel.send("This role does not exist").then(msg => {
+        msg.delete({ timeout: 30000 })
+    })
         if (member.roles.cache.get(roleAdd.id))
-            return message.channel.send(`This user already has the ${add} role`)
+            return message.channel.send(`This user already has the ${add} role`).then(msg => {
+        msg.delete({ timeout: 30000 })
+    })
         member.roles.add(roleAdd.id).then((member) => {
             message.channel.send(`${add} added to ${member.displayName}`)
         })
@@ -93,21 +115,38 @@ bot.on("message", message => {
 
     if (command === "remove") {
         if (!message.member.hasPermission('MANAGE_ROLES'))
-            return message.channel.send("Insufficient permissions (Requires permission `Manage roles`)")
+            return message.channel.send("Insufficient permissions (Requires permission `Manage roles`)").then(msg => {
+        msg.delete({ timeout: 30000 })
+    })
         const member = message.mentions.members.first()
         if (!member)
-            return message.channel.send("You have not mentioned a user")
+            return message.channel.send("You have not mentioned a user").then(msg => {
+        msg.delete({ timeout: 30000 })
+    })
         const remove = args.slice(1).join(" ")
         if (!remove)
-            return message.channel.send("You have not specified a role")
+            return message.channel.send("You have not specified a role").then(msg => {
+        msg.delete({ timeout: 30000 })
+    })
         const roleRemove = message.guild.roles.cache.find(role => role.name === remove)
         if (!roleRemove)
-            return message.channel.send("This role does not exist")
+            return message.channel.send("This role does not exist").then(msg => {
+        msg.delete({ timeout: 30000 })
+    })
         if (!member.roles.cache.get(roleRemove.id))
-            return message.channel.send(`This user does not have the ${remove} role`)
+            return message.channel.send(`This user does not have the ${remove} role`).then(msg => {
+        msg.delete({ timeout: 30000 })
+    })
         member.roles.remove(roleRemove.id).then((member) => {
             message.channel.send(`${remove} removed from ${member.displayName}`)
         })
+    }
+
+    if (command === "say") {
+    const text = args.join(" ")
+    if(!text) return message.channel.send("You have not specified something to say")
+    message.channel.send(text)
+    
     }
 
     if (command === "rps") {
